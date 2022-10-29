2da554e891c9d6b8ba337b97e59a0c9b2e2688d2
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-10-29 20:42:32 +0300

    feat: add board module and page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPagesComponent } from './shared/components/not-found-pages/not-found-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'board',
+    loadChildren: () =>
+      import('./pages/board-page/board-page.module').then(
+        (m) => m.BoardPageModule
+      ),
+  },
+  { path: '**', component: NotFoundPagesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
