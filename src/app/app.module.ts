70fbe00351cbe8533f1490cfaecabbc94c4ee6d4
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-10-31 11:33:54 +0300

    feat: add Angular Material and add the best parts of Bootstrap to Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundPagesComponent } from './shared/components/not-found-pages/not-found-pages.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NotFoundPagesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
