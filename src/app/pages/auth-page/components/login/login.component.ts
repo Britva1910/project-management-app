e7896a862b42681824517f413dbb69614768ec8b
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2022-11-02 13:44:22 +0200

    feat:add login request logic

--- src/app/pages/auth-page/components/login/login.component.ts
+++ src/app/pages/auth-page/components/login/login.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthDataService } from '../../../../shared/services/auth-data-service/auth-data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private authDataService: AuthDataService) {}
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.authDataService
+      .logIn(this.form.value)
+      .subscribe((data) => console.log(data));
+  }
+}
