c40f21545280bf8625dc4813001cc507d35d9bff
Ulugbek <u.dushanov@gmail.com>
2022-11-02 23:12:13 +0500

    feat: add header and footer components for authorized users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { appReducer } from './shared/store/app.reducer';
 import { ModalConfirmComponent } from './shared/components/modal-confirm/modal-confirm.component';
+import { HeaderAuthComponent } from './shared/components/header-auth/header-auth.component';
+import { FooterAuthComponent } from './shared/components/footer-auth/footer-auth.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { ModalConfirmComponent } from './shared/components/modal-confirm/modal-c
     HeaderComponent,
     FooterComponent,
     ModalConfirmComponent,
+    HeaderAuthComponent,
+    FooterAuthComponent,
   ],
   imports: [
     BrowserModule,
