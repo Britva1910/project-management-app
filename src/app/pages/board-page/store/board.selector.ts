bd00929270a93f5f296cfb73ea96e295e0beeee8
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-11-03 19:23:46 +0300

    feat: add selector for checked task

--- src/app/pages/board-page/store/board.selector.ts
+++ src/app/pages/board-page/store/board.selector.ts
@@ -22,3 +22,16 @@ export const selectColumnsBoard = createSelector(
   selectBoards,
   (state: StateBoard) => state.columns
 );
+
+export const selectTaskById = (idColumn: string, idTask: string) =>
+  createSelector(selectBoards, (state: StateBoard) => {
+    const columnChecked = state.columns.filter(
+      (column) => column.id == idColumn
+    );
+    if (!columnChecked[0]) return null;
+    const taskChecked = columnChecked[0].tasks.filter(
+      (task) => task.id == idTask
+    );
+    if (!taskChecked[0]) return null;
+    return taskChecked[0];
+  });
