9df40d44d239052039248a27cb239e1040ea90b0
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-11-05 19:20:38 +0300

    fix: pull origin develop

---
+++ src/app/pages/board-page/services/edit-task.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectColumnById, selectBoards } from '../store/board.selector';
+import {
+  Column,
+  Tasks,
+} from './../../../shared/models/interfaces/interfaces-board';
+
+@Injectable()
+export class EditTaskService {
+  constructor(private store: Store) {}
+
+  private isShowEditTaskModal$ = new BehaviorSubject<boolean>(false);
+
+  public showEditModal$(): Observable<boolean> {
+    return this.isShowEditTaskModal$.asObservable();
+  }
+
+  public openEditModal$() {
+    this.isShowEditTaskModal$.next(true);
+  }
+
+  public closeEditModal$() {
+    this.isShowEditTaskModal$.next(false);
+  }
+
+  public checkIdTask = '';
+
+  public checkIdColumn = '';
+
+  public checkIdBoard = '';
+
+  private checkColumn!: Column;
+
+  public checkTask!: Tasks;
+
+  private getBoardId() {
+    let board = this.store.select(selectBoards);
+    board.subscribe((col) => (this.checkIdBoard = col.id));
+  }
+
+  private getColumnById(idColumn: string) {
+    let column = this.store.select(selectColumnById(idColumn));
+    column.subscribe((col) => (this.checkColumn = col));
+  }
+
+  public editTask(idTask: string, idColumn: string) {
+    this.getBoardId();
+    this.getColumnById(idColumn);
+    const arrTaskOneColumn = this.checkColumn.tasks;
+    this.checkTask = arrTaskOneColumn.filter((task) => task.id === idTask)[0];
+    this.openEditModal$();
+  }
+}
