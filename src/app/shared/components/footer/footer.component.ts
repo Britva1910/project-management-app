f30629b915065766323fdc49d10abb978025b93c
Ulugbek <u.dushanov@gmail.com>
2022-11-06 21:18:14 +0500

    fix: resolve git conflicts

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentRoute: string;
+
+  imgSourceSv: string;
+
+  imgSourceIv: string;
+
+  imgSourceUl: string;
+
+  imgSourceRS: string;
+
+  bgColor: string;
+
+  constructor(private router: Router, private location: Location) {
+    router.events.subscribe(() => {
+      if (location.path() != '') {
+        this.currentRoute = location.path();
+        if (
+          this.currentRoute === '/welcome' ||
+          this.currentRoute === '/auth/login'
+        ) {
+          this.imgSourceSv = '../../../../assets/img/githubSv2 1.png';
+          this.imgSourceIv = '../../../../assets/img/githubIv2 1.png';
+          this.imgSourceUl = '../../../../assets/img/githubUl2 1.png';
+          this.imgSourceRS = '../../../../assets/img/logoRSdark 1.png';
+          this.bgColor = '#c2cdee';
+        } else {
+          this.imgSourceSv = '../../../../assets/img/githubSv.png';
+          this.imgSourceIv = '../../../../assets/img/githubIv.png';
+          this.imgSourceUl = '../../../../assets/img/githubUl.png';
+          this.imgSourceRS = '../../../../assets/img/rs-school.png';
+          this.bgColor = '#00093C';
+        }
+      } else {
+        this.currentRoute = 'home';
+      }
+    });
+  }
+}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  currentRoute: string;
+
+  bgColor: string;
+
+  languageButtonColor: string;
+
+  logoSource: string;
+
+  headerNavigation: boolean;
+
+  headerButtons: boolean;
+
+  constructor(private router: Router, private location: Location) {
+    router.events.subscribe(() => {
+      if (location.path() != '') {
+        this.currentRoute = location.path();
+        if (
+          this.currentRoute === '/welcome' ||
+          this.currentRoute === '/auth/login'
+        ) {
+          this.logoSource = '../../../../assets/img/logo.png';
+          this.bgColor = '#c2cdee';
+          this.languageButtonColor = '#000';
+          this.headerNavigation = false;
+          this.headerButtons = true;
+        } else {
+          this.logoSource = '../../../../assets/img/light-logo.png';
+          this.bgColor = '#00093c';
+          this.languageButtonColor = '#fff';
+          this.headerNavigation = true;
+          this.headerButtons = false;
+        }
+      } else {
+        this.currentRoute = 'home';
+      }
+    });
+  }
+}
