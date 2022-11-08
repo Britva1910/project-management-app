fd43fcf80c9fbbb6f5db09dd5d5c69bf2428bdc8
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2022-11-08 17:22:32 +0200

    feat: change login operations order

--- src/app/pages/auth-page/services/login.service.ts
+++ src/app/pages/auth-page/services/login.service.ts
@@ -1,21 +1,58 @@
 import { Injectable } from '@angular/core';
 import { UserDataService } from '../../../shared/services/user-data-service/user-data.service';
-import { map } from 'rxjs';
+import { LocalStorageService } from '../../../shared/services/local-storage-service/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private userDataService: UserDataService) {}
+  constructor(
+    private userDataService: UserDataService,
+    private localStorageService: LocalStorageService
+  ) {}
 
-  getUserId(userName: string | null | undefined) {
-    return this.userDataService.getAllUsers().pipe(
-      map((response) => {
-        return response.filter((item) =>
-          item.name === userName ? item : null
-        );
-      }),
-      map((data) => data[0].id)
+  isLogin(): boolean {
+    const token = this.getTokenFromLocalStorage();
+    //const token = //this is expires token for testing
+    //('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjMzk3MzFlMy0zZDhmLTQ1OGMtYjVmMi1lZmNjYTI3MmRlNzciLCJsb2dpbiI6InVzZXIwMDE3IiwiaWF0IjoxNjY2OTkxODYwfQ.HXxnT-2EdjuriLr82_uUK8M7m-5V8RTwNBDgpDtsCJM');
+    if (token) {
+      console.log('start login fn');
+      const userId = this.getUserIdFromToken(token);
+      const response = this.userDataService.getUserById(userId);
+
+      response.subscribe({
+        next: () => {
+          return true;
+        },
+        error: () => {
+          return false;
+        },
+      });
+    }
+    return true;
+  }
+
+  getTokenFromLocalStorage(): string | null {
+    const tokenFromLocalStorage =
+      this.localStorageService.getFromLocalStorage('token');
+    if (typeof tokenFromLocalStorage === 'string') {
+      return tokenFromLocalStorage;
+    }
+    return null;
+  }
+
+  getUserIdFromToken(token: string): string {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
     );
+    return JSON.parse(jsonPayload).userId;
   }
 }
