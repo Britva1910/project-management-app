5e481a9bca9fd3d5ebc6d3ca020652a81c1ada48
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-11-22 23:46:22 +0300

    feat: add block-vodeo

---
+++ src/app/pages/welcome-page/components/video-player/video-player.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss'],
+})
+export class VideoPlayerComponent implements OnInit {
+  videoId: string = 'cYsgQGJJqQE';
+
+  w = 350;
+
+  h = 200;
+
+  ngOnInit() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
+}
